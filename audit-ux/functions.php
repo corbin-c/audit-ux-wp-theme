ddd5fc2b8e9ab7f9d559547337995f390c52b348
Clément Corbin <corbin.clement@gmail.com>
2021-05-07 08:15:06 +0200

    typo fix

--- audit-ux/functions.php
+++ audit-ux/functions.php
@@ -132,6 +132,7 @@ function audit_ux_widgets_init() {
       'before_title'  => '<h2 class="widget-title">',
       'after_title'   => '</h2>',
     )
+  );
   register_sidebar(
     array(
       'name'          => esc_html__( 'Newsletter', 'audit-ux' ),
